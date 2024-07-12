956b02a88ecde93d263662f38f3bbf68ce6d8b43
Ricardo Villalta <sidernes22@gmail.com>
2024-07-12 12:08:22 -0600

    feat: add model ProductTransaction

---
+++ src/main/java/org/rvillalta/jdbc/model/ProductTransaction.java
@@ -0,0 +1,61 @@
+package org.rvillalta.jdbc.model;
+
+import java.sql.Timestamp;
+
+public class ProductTransaction {
+    private int transactionId;
+    private int productId;
+    private int quantity;
+    private String transactionType;
+    private Timestamp transactionDate;
+    private String description;
+
+    // Getters y Setters
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public Timestamp getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Timestamp transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
