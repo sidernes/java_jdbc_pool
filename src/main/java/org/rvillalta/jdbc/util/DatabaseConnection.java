d64fe05174f8c4be04b15b1f1f4a44fd8ee4962a
Ricardo Villalta <sidernes22@gmail.com>
2024-07-12 17:37:21 -0600

    🐛 Fix: Connecion autoclose problem

--- src/main/java/org/rvillalta/jdbc/util/DatabaseConnection.java
+++ src/main/java/org/rvillalta/jdbc/util/DatabaseConnection.java
@@ -5,17 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static String url = "jdbc:mysql://127.0.0.1:3306/java_curso?serverTimezone=GMT-6";
-    private static String username = "root";
-    private static String password = "my-secret-pw";
-    private static Connection connection;
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/java_curso?serverTimezone=GMT-6";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "my-secret-pw";
 
-    public static Connection getConnection() throws SQLException{
-        if(connection == null){
-            connection = DriverManager.getConnection(url, username, password);
-        }
-
-        return connection;
-
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
