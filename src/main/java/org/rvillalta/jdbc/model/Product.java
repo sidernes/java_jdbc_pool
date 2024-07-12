98a9b0302f04d92a1b125d8d67191a560a8e2f9f
Ricardo Villalta <sidernes22@gmail.com>
2024-07-12 12:48:39 -0600

    🔨 Feat add: category fiel to Product model

--- src/main/java/org/rvillalta/jdbc/model/Product.java
+++ src/main/java/org/rvillalta/jdbc/model/Product.java
@@ -3,11 +3,13 @@ package org.rvillalta.jdbc.model;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
+
 public class Product {
     private int id;
     private String name;
     private BigDecimal price;
     private Timestamp dateRegistered;
+    private int categoryId;
 
     // Getters y Setters
     public int getId() {
@@ -40,5 +42,13 @@ public class Product {
 
     public void setDateRegistered(Timestamp dateRegistered) {
         this.dateRegistered = dateRegistered;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 }
